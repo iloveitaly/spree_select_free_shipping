148f9ea09e3a6977029ef16d673ae923f80ad1aa
Michael Bianco <iloveitaly@gmail.com>
2014-07-13 12:56:13 -0400

    Improving specs to test promotion ordering

--- spec/models/free_shipping_selection_spec.rb
+++ spec/models/free_shipping_selection_spec.rb
@@ -29,4 +29,33 @@ describe Spree::Calculator::FreeShippingSelection do
 
   	expect(free_shipping_delivery.available?(order)).to be_true
   end
+
+  it "should be available when there is two adjustments associated with a promotion" do
+  	promo = Spree::Promotion.create(:name => "free shipping")
+
+  	flat_rate = Spree::Calculator::FlatRate.create
+  	flat_rate.preferred_amount = 1.0
+  	Spree::Promotion::Actions::CreateAdjustment.create({
+  		promotion: promo,
+  		calculator: flat_rate
+  	}, without_protection: true)
+
+  	# the order here matters; the free shipping selection must come last
+  	Spree::Promotion::Actions::CreateAdjustment.create({
+  		promotion: promo,
+  		calculator: Spree::Calculator::FreeShippingSelection.new
+  	}, without_protection: true)
+
+  	promo.reload
+  	promo.activate({ order: order })
+
+  	variant = FactoryGirl.create(:variant, price: 51.00)
+  	order.add_variant(variant, 2)
+  	promo.activate({ order: order })
+
+  	expect(order.total.to_f).to eq(101.00)
+  	expect(order.adjustments.promotion).to_not be_empty
+  	expect(promo.actions.size).to eq(2)
+  	expect(free_shipping_delivery.available?(order)).to be_true
+  end
 end
